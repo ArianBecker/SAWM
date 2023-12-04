0c4c182e34334ec29b6ba636841506a44a2108a0
ArianBecker <121944242+ArianBecker@users.noreply.github.com>
2023-12-04 18:27:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+title = document.getElementById("title");
+story = "Our Story";
+
+title.innerText = "";
+
+setTimeout(function () {title.innerText = "O"}, 300)
+setTimeout(function () {title.innerText = "Ou"}, 500)
+setTimeout(function () {title.innerText = "Our"}, 800)
+setTimeout(function () {title.innerText = "Our "}, 1200)
+setTimeout(function () {title.innerText = "Our S"}, 1200)
+setTimeout(function () {title.innerText = "Our St"}, 1500)
+setTimeout(function () {title.innerText = "Our Sto"}, 1600)
+setTimeout(function () {title.innerText = "Our Stor"}, 2000)
+setTimeout(function () {title.innerText = "Our Story"}, 2600)
