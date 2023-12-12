9083929656b00d43658c6a7b1adb80f98e46af2f
ArianBecker <121944242+ArianBecker@users.noreply.github.com>
2023-12-12 09:30:10 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-title = document.getElementById("title");
-story = "Our Story";
-
-title.innerText = "";
-
-setTimeout(function () {title.innerText = "O"}, 1300)
-setTimeout(function () {title.innerText = "Ou"}, 1500)
-setTimeout(function () {title.innerText = "Our"}, 1800)
-setTimeout(function () {title.innerText = "Our "}, 2200)
-setTimeout(function () {title.innerText = "Our S"}, 2200)
-setTimeout(function () {title.innerText = "Our St"}, 2500)
-setTimeout(function () {title.innerText = "Our Sto"}, 2600)
-setTimeout(function () {title.innerText = "Our Stor"}, 3000)
-setTimeout(function () {title.innerText = "Our Story"}, 3600)
+var title = $("#title"); 
+title.fadeIn(); 
